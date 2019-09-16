40870979ac0c2cdca8a22d6272fd7f2106e08757
Gian Luca <luca.gian16@outlook.com>
2019-09-16 19:47:03 -0300

    [AB#77]
    Corrigida erro no subshell.

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-( cd ../DeadFishStudio.Product.Domain.Model/ && dotnet tool install --global coverlet.console && dotnet restore && dotnet build )
+( cd ../DeadFishStudio.Product.Domain.Model/ && dotnet restore && dotnet build )
 
-( cd ../DeadFishStudio.Product.Infrastructure.Data/ && dotnet tool install --global coverlet.console && dotnet restore && dotnet build )
+( cd ../DeadFishStudio.Product.Infrastructure.Data/ && dotnet restore && dotnet build )
 
-( cd ../DeadFishStudio.Product.Domain.Service/ && dotnet tool install --global coverlet.console && dotnet restore && dotnet build )
+( cd ../DeadFishStudio.Product.Domain.Service/ && dotnet restore && dotnet build )
 
-( cd ../DeadFishStudio.Product.Infrastructure.CrossCutting/ && dotnet tool install --global coverlet.console && dotnet restore && dotnet build )
+( cd ../DeadFishStudio.Product.Infrastructure.CrossCutting/ && dotnet restore && dotnet build )
 
-(cd ../DeadFishStudio.Product.Application.Api/ && dotnet tool install --global coverlet.console && dotnet restore && dotnet build )
+( cd ../DeadFishStudio.Product.Application.Api/  && dotnet restore && dotnet build )
