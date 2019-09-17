82a6322e702809ad4665a216833ad2605a46f4d2
Gian Luca <luca.gian16@outlook.com>
2019-09-17 00:06:43 -0300

    [AB#79] [AB#89]
    - Criada entidade de Produto
    Criado objeto de valor do Preco
    
    Fixes AB#79 and Fixes AB#89

---
+++ DeadFishStudio.Product.Domain.Model/Entity/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DeadFishStudio.Product.Domain.Model.ObjectOfValue;
+using GianLuca.Domain.Core.Entity;
+
+namespace DeadFishStudio.Product.Domain.Model.Entity
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public Price Price { get; set; }
+    }
+}
