18980d10c1a1ac9ad8e86f31758ca8f3aec8e7f9
Gian Luca <luca.gian16@outlook.com>
2019-09-17 17:10:09 -0300

    [AB#90]
    Criada base do contexto do projeto de produto.

---
+++ DeadFishStudio.Product.Infrastructure.Data/Context/ProductContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DeadFishStudio.Product.Infrastructure.Data.Context.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeadFishStudio.Product.Infrastructure.Data.Context
+{
+    public class ProductContext: DbContext
+    {
+        public const string DefaultSchema = "deadfish";
+
+        public virtual DbSet<Domain.Model.Entity.Product> ProductDbSet { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            _ = builder.HasDefaultSchema(DefaultSchema);
+            _ = builder.ApplyConfiguration(new ProductConfiguration());
+            _ = builder.ApplyConfiguration(new PriceConfiguration());
+            base.OnModelCreating(builder);
+        }
+    }
+}
