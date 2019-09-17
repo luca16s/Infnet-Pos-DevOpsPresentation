b1ec2a602d6bf8e9d58623d78784de9760a983d8
Gian Luca da Silva Figueiredo <luca.gian16@outlook.com>
2019-09-17 17:27:33 -0300

    Develop (#19)
    * [AB#62] (#3)
    
    * [AB#62]
    
    Criada a Solution do Projeto.
    
    * [AB#67] (#4)
    
    Criado projeto de dominio do produto.
    
    * [AB#68] (#5)
    
    Criada camada de servico de dominio.
    
    AB#68
    
    * [AB#69] (#6)
    
    Criado projeto para banco de dados.
    
    Fixes AB#69
    
    * [AB#70] (#7)
    
    Criado projeto de CrossCutting do Produto.
    
    * [AB#71] (#8)
    
    Criada API de Produto.
    
    Fixed AB#71
    
    * [AB#77] (#9)
    
    * [AB#77]
    
    Criados arquivos para se integrar com o TravisCI
    
    Fixed AB#77
    
    * [AB#77]
    
    Corrige o problema de ficar voltando na pasta com cd.
    
    #L6
    
    * [AB#77]
    
    Corrigido #L3.
    
    * [AB#77]
    
    Atualizado sdk do .net para o build no arquivo .travis.yml
    
    * [AB#77]
    
    Adicionada Distro no .travis.yml
    
    * [AB#77]
    
    Corrigida termo dist duplicado.
    
    * [AB#77]
    
    Corrigido nome da Solution errado.
    
    * [AB#77]
    
    Alterado target do projeto.
    
    * [AB#77]
    
    Alterada a dist do projeto.
    
    * [AB#77]
    
    Alterado arquivo .travis
    
    * [AB#77]
    
    Corrigida erro no subshell.
    
    * [AB#77]
    
    Comentados scripts que nao serao utilizados no momento.
    
    * Feature/ab#86 (#12)
    
    * Develop (#10)
    
    * [AB#62] (#3)
    
    * [AB#62]
    
    Criada a Solution do Projeto.
    
    * [AB#67] (#4)
    
    Criado projeto de dominio do produto.
    
    * [AB#68] (#5)
    
    Criada camada de servico de dominio.
    
    AB#68
    
    * [AB#69] (#6)
    
    Criado projeto para banco de dados.
    
    Fixes AB#69
    
    * [AB#70] (#7)
    
    Criado projeto de CrossCutting do Produto.
    
    * [AB#71] (#8)
    
    Criada API de Produto.
    
    Fixed AB#71
    
    * [AB#77] (#9)
    
    * [AB#77]
    
    Criados arquivos para se integrar com o TravisCI
    
    Fixed AB#77
    
    * [AB#77]
    
    Corrige o problema de ficar voltando na pasta com cd.
    
    #L6
    
    * [AB#77]
    
    Corrigido #L3.
    
    * [AB#77]
    
    Atualizado sdk do .net para o build no arquivo .travis.yml
    
    * [AB#77]
    
    Adicionada Distro no .travis.yml
    
    * [AB#77]
    
    Corrigida termo dist duplicado.
    
    * [AB#77]
    
    Corrigido nome da Solution errado.
    
    * [AB#77]
    
    Alterado target do projeto.
    
    * [AB#77]
    
    Alterada a dist do projeto.
    
    * [AB#77]
    
    Alterado arquivo .travis
    
    * [AB#77]
    
    Corrigida erro no subshell.
    
    * [AB#77]
    
    Comentados scripts que nao serao utilizados no momento.
    
    * Set up CI with Azure Pipelines
    
    [skip ci]
    
    * [AB#86]
    
    Adicionado o mantenedor do projeto.
    
    * [AB#86]
    
    Alterada a label de maintainer e adicionada a label de versao.
    
    * [AB#86]
    
    Removido espaco do arquivo.
    
    * [AB#86]
    
    Alterqado Path para o csproject no DOCKERFILE.
    
    * [AB#86]
    
    Alteracao do azure pipelines
    
    * Set up CI with Azure Pipelines (#13)
    
    [skip ci]
    
    * [AB#86]
    
    Trocado o path do projeto.
    
    * [AB#86]
    
    Alteracao do path.
    
    * [AB#86]
    
    Testando System.DefaultWorkingDirectory
    
    * [AB#86]
    
    Alteracao nos inputs do azure devops
    
    * [AB#86]
    
    Removido base path
    
    * [AB#86]
    
    Corrigido o DOCKERFILE
    
    * [AB#79] [AB#89] (#15)
    
    - Criada entidade de Produto
    Criado objeto de valor do Preco
    
    Fixes AB#79 and Fixes AB#89
    
    * [AB#64] (#17)
    
    Incluidas as interfaces de repositorio e servico.
    
    Fixed AB#80.
    Fixed AB#81.
    
    * Feature/ab#66 (#18)
    
    * [AB#91]
    
    Criadas as classes de configuracao do projeto.
    
    Fixed: AB#91
    
    * [AB#66]
    
    Alterado nome da variavel referente ao preco.
    
    * [AB#90]
    
    Criada base do contexto do projeto de produto.
    
    * [AB#86]
    
    Removidas diretivas desnecessarias.

---
+++ DeadFishStudio.Product.Domain.Model/Interfaces/Services/IProductServiceAsync.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GianLuca.Domain.Core.Interfaces.Services;
+
+namespace DeadFishStudio.Product.Domain.Model.Interfaces.Services
+{
+    public interface IProductServiceAsync : IBaseServiceAsync<Entity.Product>
+    {
+    }
+}
---
+++ DeadFishStudio.Product.Infrastructure.Data/Context/Configuration/PriceConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DeadFishStudio.Product.Infrastructure.Data.Context.Configuration
+{
+    public class PriceConfiguration : IEntityTypeConfiguration<Domain.Model.ObjectOfValue.Price>
+    {
+        public void Configure(EntityTypeBuilder<Domain.Model.ObjectOfValue.Price> builder)
+        {
+            builder
+                .ToTable("PRICE");
+
+            builder
+                .Property(price => price.Currency)
+                .HasColumnName("PRCE_CURRENCY");
+
+            builder
+                .Property(price => price.Amount)
+                .HasColumnName("PRCE_AMOUNT");
+        }
+    }
+}
