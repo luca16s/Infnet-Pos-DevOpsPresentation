f92ba333c63493deec4c3b6b317b315c464b2047
Gian Luca <luca.gian16@outlook.com>
2019-09-17 18:07:43 -0300

    [AB#92]
    Criada a implementacao dos repositorios.
    
    Fixed: AB#92

---
+++ DeadFishStudio.Product.Infrastructure.Data/Repositories/BaseRepositoryAsync.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DeadFishStudio.Product.Infrastructure.Data.Context;
+using GianLuca.Domain.Core.Entity;
+using GianLuca.Domain.Core.Interfaces.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeadFishStudio.Product.Infrastructure.Data.Repositories
+{
+    public class BaseRepositoryAsync<T> : IBaseRepositoryAsync<T> where T : BaseEntity
+    {
+        protected ProductContext Context;
+        protected DbSet<T> DbSet { get; }
+
+        public BaseRepositoryAsync(ProductContext productContext)
+        {
+            Context = productContext;
+            DbSet = Context.Set<T>();
+        }
+
+        public async Task<T> AddItemAsync(T item)
+        {
+            try
+            {
+                var entity = await DbSet.AddAsync(item);
+                entity.State = EntityState.Added;
+                return entity.Entity;
+            }
+            catch (DbUpdateException e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public async Task<T> GetItemAsync(Guid id)
+        {
+            return await DbSet.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<T>> GetAllItemsAsync()
+        {
+            return await DbSet.ToListAsync();
+        }
+
+        public T UpdateItem(Guid id, T item)
+        {
+            var entry = DbSet.Update(item);
+            entry.State = EntityState.Modified;
+            return entry.Entity;
+        }
+
+        public void DeleteItem(T item)
+        {
+            DbSet.Remove(item);
+        }
+    }
+}
---
+++ DeadFishStudio.Product.Infrastructure.Data/Repositories/ProductRepositoryAsync.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DeadFishStudio.Product.Domain.Model.Interfaces.Repositories;
+using DeadFishStudio.Product.Infrastructure.Data.Context;
+
+namespace DeadFishStudio.Product.Infrastructure.Data.Repositories
+{
+    public class ProductRepositoryAsync : BaseRepositoryAsync<Domain.Model.Entity.Product>, IProductRepositoryAsync
+    {
+        public ProductRepositoryAsync(ProductContext productContext) : base(productContext)
+        {
+        }
+    }
+}
