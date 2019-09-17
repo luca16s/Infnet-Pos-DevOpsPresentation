83375ace2c049419987931c5f9657198840e92f5
Gian Luca <luca.gian16@outlook.com>
2019-09-17 17:04:31 -0300

    [AB#91]
    Criadas as classes de configuracao do projeto.
    
    Fixed: AB#91

---
+++ DeadFishStudio.Product.Infrastructure.Data/Context/Configuration/PriceConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DeadFishStudio.Product.Infrastructure.Data.Context.Configuration
+{
+    public class PriceConfiguration : IEntityTypeConfiguration<Domain.Model.ObjectOfValue.Price>
+    {
+        public void Configure(EntityTypeBuilder<Domain.Model.ObjectOfValue.Price> builder)
+        {
+            builder
+                .ToTable("PRICE");
+
+            builder
+                .Property(price => price.Currency)
+                .HasColumnName("PRCE_CURRENCY");
+
+            builder
+                .Property(price => price.Amount)
+                .HasColumnName("PRCE_AMOUNT");
+        }
+    }
+}
---
+++ DeadFishStudio.Product.Infrastructure.Data/Context/Configuration/ProductConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DeadFishStudio.Product.Infrastructure.Data.Context.Configuration
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Domain.Model.Entity.Product>
+    {
+        public void Configure(EntityTypeBuilder<Domain.Model.Entity.Product> builder)
+        {
+            builder
+                .ToTable("PRODUCT");
+
+            builder.HasKey(product => product.Id);
+
+            builder
+                .Property(product => product.Id)
+                .HasColumnName("PDCT_SQ_PRODUCT");
+
+            builder
+                .Property(product => product.Name)
+                .HasColumnName("PDCT_NAME");
+
+            builder
+                .Property(product => product.Quantity)
+                .HasColumnName("PDCT_QUANTITY");
+
+            builder
+                .OwnsOne<Domain.Model.Entity.Product>("Price", price =>
+                {
+                    price.HasForeignKey("PDCT_SQ_PRODUCT");
+                    price.HasKey("PDCT_SQ_PRODUCT");
+                });
+
+            builder
+                .Ignore(product => product.Notifications);
+        }
+    }
+}
