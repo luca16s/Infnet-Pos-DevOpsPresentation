2930f6413b8795f83b9cbfcbd1cec55cbb090214
Gian Luca da Silva Figueiredo <luca.gian16@outlook.com>
2019-09-17 18:00:15 -0300

    AB#82 (#21)
    * [AB#83]
    
    Criar o metodo de extensao do banco.
    
    Fixed: AB#83
    
    * [AB#83]
    
    Criado o arquivo de injecao de dependencia.
    
    Fixed: AB#83
    
    * [AB#94]
    
    Criado o arquivo de configuracao com a connection string do produto.
    
    Fixed: AB#94

---
+++ DeadFishStudio.Product.Infrastructure.CrossCutting/CustomExtensionsMethods.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DeadFishStudio.Product.Infrastructure.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MySql.Data.EntityFrameworkCore.Extensions;
+
+namespace DeadFishStudio.Product.Infrastructure.CrossCutting
+{internal static class CustomExtensionsMethods
+    {
+        public static IServiceCollection AddCustomDbContext(this IServiceCollection services, ProductConfiguration configuration)
+        {
+            if (configuration == null) throw new ArgumentNullException(nameof(configuration));
+
+            services.AddEntityFrameworkMySQL()
+                .AddDbContext<ProductContext>(options =>
+                {
+                    options
+                        .UseMySQL(configuration.ConnectionString);
+                });
+
+            return services;
+        }
+    }
+}
