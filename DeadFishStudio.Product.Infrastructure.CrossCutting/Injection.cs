67f05e78aa2236f353bd79223a04f9629bd49fa7
Gian Luca <luca.gian16@outlook.com>
2019-09-17 17:54:21 -0300

    [AB#83]
    Criado o arquivo de injecao de dependencia.
    
    Fixed: AB#83

---
+++ DeadFishStudio.Product.Infrastructure.CrossCutting/Injection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DeadFishStudio.Product.Domain.Model.Interfaces.Repositories;
+using DeadFishStudio.Product.Domain.Model.Interfaces.Services;
+using DeadFishStudio.Product.Infrastructure.Data.Context;
+using GianLuca.Domain.Core.Interfaces.UnitOfWork;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DeadFishStudio.Product.Infrastructure.CrossCutting
+{
+    public static class Injection
+    {
+        public static void Services(IServiceCollection services, IConfiguration configuration)
+        {
+            var productConfig = new ProductConfiguration();
+            configuration.GetSection(nameof(ProductConfiguration)).Bind(productConfig);
+
+            //services.AddTransient(typeof(IProductRepositoryAsync), typeof(ProductRepositoryAsync));
+            //services.AddTransient(typeof(IProductServiceAsync), typeof(ProductServiceAsync));
+            services.AddTransient(typeof(IUnitOfWork), typeof(ProductContext));
+            services.AddCustomDbContext(productConfig);
+        }
+    }
+}
