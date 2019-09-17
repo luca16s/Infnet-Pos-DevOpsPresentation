9e70ffbdbd5359ae347b9c2f54106828db71a911
Gian Luca <luca.gian16@outlook.com>
2019-09-17 16:25:40 -0300

    [AB#64]
    Incluidas as interfaces de repositorio e servico.
    
    Fixed AB#80.
    Fixed AB#81.

---
+++ DeadFishStudio.Product.Domain.Model/Interfaces/Repositories/IProductRepositoryAsync.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GianLuca.Domain.Core.Interfaces.Repository;
+
+namespace DeadFishStudio.Product.Domain.Model.Interfaces.Repositories
+{
+    public interface IProductRepositoryAsync : IBaseRepositoryAsync<Entity.Product>
+    {
+    }
+}
